c1857b4e004d7efd663f732659e93c2eb773a92e
Jason Inouye <jasonjinouye@gmail.com>
2023-10-27 14:01:36 -0500

    jswebtoken logic added

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRoutes = require('./routes/authRoutes');
+const requireAuth = require('./middlewares/requireAuth');
 
 const app = express();
 
@@ -20,8 +21,8 @@ mongoose.connection.on('error', (err) => {
     console.error('Error connection to Mongo', err )
 })
 
-app.get('/', (req, res) => {
-    res.send('Hi There!');
+app.get('/', requireAuth, (req, res) => {
+    res.send(`Your Email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
